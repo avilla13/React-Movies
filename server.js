c0fb2f6be578eb5ad03ce3db1832f8284de2c198
avilla13 <xetox13@gmail.com>
2023-08-11 16:55:55 -0500

    Install mongoose, set up .env dB, and connect express to db

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 // Always require and configure near the top
-// require('dotenv').config();
+require('dotenv').config();
 // Connect to the database
-// require('./config/database');
+require('./config/database');
 
 const app = express();
 
