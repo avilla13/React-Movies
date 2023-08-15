fcd7d84f70a3419b7805ae1c5126b8bcbf5f4288
avilla13 <xetox13@gmail.com>
2023-08-14 21:36:48 -0500

    Add & display movie info for MovieDetailPage

--- src/pages/MovieDetailPage/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage/MovieDetailPage.jsx
@@ -1,6 +1,28 @@
+import { useParams } from "react-router-dom";
+import './MovieDetailPage.css';
 
-export default function MovieDetailPage() {
+export default function MovieDetailPage({movies}) {
+  let { movieName } = useParams();
+  const movie = movies.find((movie) => movie.title === movieName);
+  
   return (
-    <div>MovieDetailPage</div>
+    <div >
+      <h1>Movie Detail Page</h1>
+      <div className="detail-grid">
+        <div className="movie-details">
+          <h1>{movie.title}</h1>
+          <h3>Released: {movie.releaseDate}</h3>
+          <h3>Cast Members: </h3>
+          <ul>
+            {movie.cast.map((castMember, idx) => (
+              <li key={idx}>{castMember}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <img src={movie.posterPath} alt={movie.title} />
+        </div>
+      </div>
+    </div>
   )
 }
