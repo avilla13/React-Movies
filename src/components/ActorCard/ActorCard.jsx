2d11e6e11c306eca542b1707349dc714749a8de6
avilla13 <xetox13@gmail.com>
2023-08-16 20:36:49 -0500

    Style ActorCard with random color

--- src/components/ActorCard/ActorCard.jsx
+++ src/components/ActorCard/ActorCard.jsx
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 import './ActorCard.css';
 
 export default function ActorCard({actor}) {
-    
-    return (
-        <>
-            <div className="actor-card" >
-                <div className="actor-info">
-                    <h2>{actor}</h2>                    
-                </div>
-            </div>            
-        </>
-    )
+  // generate random color with hexadecimal code
+  function generateRandomColor(){
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+  }
+
+  return (
+    <>
+      <div className="actor-card" style={{ backgroundColor: generateRandomColor() }} >
+        <div className="actor-info">
+          <h2>{actor}</h2>                    
+        </div>
+      </div>            
+    </>
+  )
 }
